b7e5683ac585dbf3f18de05fd9faa4a59ca64fbf
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-21 11:57:59 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите пятизначное число");
+var num = Convert.ToInt32(Console.ReadLine());
+if (num < 10000 || num > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+    if (num % 10 == num / 10000 && num / 10 % 10 == num / 1000 % 10)
+        {
+            Console.WriteLine("Палиндром");
+        }
+    else 
+    Console.WriteLine("Не палиндром");
+}
